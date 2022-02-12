7a0db9a7d6525846262511050cdf5e72f88a6876
maanHimself <maanasdf87@gmail.com>
2022-02-12 15:20:12 +0530

    start

---
+++ static/m042Ftk63PDsg_nSikVZf/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/94-61340954e92240b5.js","static/chunks/pages/index-7e12239e2b7359a9.js"],"/_error":["static/chunks/pages/_error-2280fa386d040b66.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
